03254eb4aa4a636808f95cde84b5e6425d28495b
Juan <juan@seasonmint.com>
2020-04-12 19:00:19 -0400

    "add stars event"

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -6,4 +6,7 @@ public class Defender : MonoBehaviour
 {
     // Start is called before the first frame update
    [SerializeField] int starCost = 100;
+   public void AddStars(int amount){
+       FindObjectOfType<StarDisplay>().AddStars(amount);
+   }
 }
